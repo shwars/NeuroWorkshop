a7db875bebc7a34524cd683cf7b9739f6248f690
Dmitri Soshnikov <dmitri@soshnikov.com>
2017-05-24 12:17:00 +0300

    GZip all data

---
+++ Utils/convert_mnist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import numpy as np
+import pickle
+
+ftrain = '../Data/MNIST/train.csv'
+fpick = '../Data/MNIST/mnist.pkl'
+def mnist_lines(fn):
+    with open(fn) as f:
+        f.readline()
+        return f.readlines()
+
+tr_lines = np.array([ [ int(z) for z in x.split(',')] for x in mnist_lines(ftrain)[1:10]])
+
+MNIST = {}
+
+MNIST['Train'] = {}
+MNIST['Train']['Labels'] = tr_lines[:,0]
+MNIST['Train']['Features'] = tr_lines[:,1:]
+
+with open(fpick,'wb') as f:
+    f.write(pickle.dump(MNIST,f))
