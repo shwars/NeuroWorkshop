a5f75f507bfa42252d14fca2716ac3b1aaade98c
Dmitri Soshnikov <dmitri@soshnikov.com>
2017-11-26 22:04:55 +0300

    Add image

--- Python/text-dnn.py
+++ Python/text-dnn.py
@@ -5,21 +5,18 @@ import sys
 import cntk as C
 from cntk import Trainer, Axis
 from cntk.learners import momentum_sgd, momentum_as_time_constant_schedule, learning_rate_schedule, UnitType
-from cntk.ops import sequence
 from cntk.losses import cross_entropy_with_softmax
 from cntk.metrics import classification_error
-from cntk.ops.functions import load_model
 from cntk.layers import LSTM, Stabilizer, Recurrence, Dense, For, Sequential
 from cntk.logging import log_number_of_parameters, ProgressPrinter
-from scipy.ndimage.interpolation import shift
 
-data = open("d:\\work\\NeuroWorkshop\\Data\\texts\\Alice.txt", "r",encoding="utf-8").read()
+data = open("d:\\work\\NeuroWorkshop\\Data\\texts\\Alice.txt",encoding="utf-8").read()
+data = data[0:len(data)//10]
 chars = sorted(list(set(data)))
 data_size, vocab_size = len(data), len(chars)
 print('data has %d characters, %d unique.' % (data_size, vocab_size))
 char_to_ix = { ch:i for i,ch in enumerate(chars) }
 ix_to_char = { i:ch for i,ch in enumerate(chars) }
-
 
 nchars=100
 def get_sample(p):
@@ -36,36 +33,26 @@ get_sample(0)
 input_text = C.input_variable((nchars,vocab_size))
 output_char = C.input_variable(shape=vocab_size)
 
-model = Sequential([Dense(3000),Dense(vocab_size)])
+model = Sequential([Dense(3000,activation=C.relu),Dense(vocab_size,activation=None)])
 
 z = model(input_text)
 
 ce = cross_entropy_with_softmax(z, output_char)
 errs = classification_error(z, output_char)
 
-lr_per_sample = learning_rate_schedule(0.001, UnitType.sample)
+lr_per_sample = learning_rate_schedule(0.01, UnitType.minibatch)
 momentum_time_constant = momentum_as_time_constant_schedule(1100)
-clipping_threshold_per_sample = 5.0
-gradient_clipping_with_truncation = True
-learner = momentum_sgd(z.parameters, lr_per_sample, momentum_time_constant,
-                    gradient_clipping_threshold_per_sample=clipping_threshold_per_sample,
-                    gradient_clipping_with_truncation=gradient_clipping_with_truncation)
+learner = C.learners.adam(z.parameters, lr_per_sample,momentum=momentum_time_constant)
 progress_printer = ProgressPrinter(freq=100, tag='Training')
 trainer = Trainer(z, (ce, errs), learner, progress_printer)
     
 log_number_of_parameters(z)
 
-a,b = get_sample(0)
-print("a={}, b={}".format(a.shape,b.shape))
-
 for ep in range(1):
     print("Epoch={}".format(ep))
-    m = [True]
-    for mb in range(data_size-nchars-1):
+    for mb in range(0,data_size-nchars-1):
         feat,lab = get_sample(mb)
         trainer.train_minibatch({input_text: feat, output_char: lab})
-        m=[False]
-
 
 def sample(net, prime_text='', use_hardmax=True, length=100, temperature=1.0):
 
@@ -87,6 +74,8 @@ def sample(net, prime_text='', use_hardmax=True, length=100, temperature=1.0):
 
     if prime_text=='': prime_text = data[0:nchars]
 
+    if (len(prime_text)<nchars): prime_text = " "*(nchars-len(prime_text))+prime_text
+
     out = prime_text+"|";
 
     inp = np.eye(vocab_size,dtype=np.float32)[np.array([char_to_ix[x] for x in prime_text])]
@@ -100,4 +89,4 @@ def sample(net, prime_text='', use_hardmax=True, length=100, temperature=1.0):
         inp[-1,:] = np.eye(vocab_size,dtype=np.float32)[ochr]
     return out
 
-sample(z, '', True)
+sample(C.softmax(z), 'A quick brown fox jumped over the lazy sleeping dog. While I was reading this text, something happen',length=300,use_hardmax=True)
